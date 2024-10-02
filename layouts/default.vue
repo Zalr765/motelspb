60b92a5087b09b8eccc52a47c445d97cb5120725
Sattsaev Zaur <o2@MacBook-Pro-Sattsaev.local>
2024-10-02 11:02:31 +0300

    add fonts and scss

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+	<div class="default-layout">
+		<Header />
+		<main>
+			<slot />
+		</main>
+	</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>sss</div>
+	<div>ssss</div>
 </template>
 
 <script setup>
