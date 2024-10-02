e27c793197f77c52862ff18f0aaf9b3495e6b213
Sattsaev Zaur <o2@MacBook-Pro-Sattsaev.local>
2024-10-02 12:49:13 +0300

    TSK-3406

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+ compatibilityDate: '2024-04-03',
+ devtools: { enabled: true },
+ css: ['@/assets/scss/main.scss'],
 
-  devtools: { enabled: true },
-
-  css: ['@/assets/scss/main.scss'],
-
-  vite: {
-		css: {
-			preprocessorOptions: {
-				scss: {
-					additionalData: `@import "@/assets/scss/base/variables.scss";`,
-          api: 'modern-compiler'
-				},
+ vite: {
+							css: {
+											preprocessorOptions: {
+															scss: {
+																			additionalData: `@import "@/assets/scss/base/variables.scss";`,
+									api: 'modern-compiler'
+															},
+											},
+							},
 			},
-		},
-	},
+
+ modules: ['@pinia/nuxt'],
 })
